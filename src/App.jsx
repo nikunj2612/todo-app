669b8a6840c66e135b347ce8ff0e0c4279e11b6f
astics <nikunj.tandel@asticsinc.com>
2024-06-25 14:22:39 +0530

    removed unwanted components

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,13 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import Popup from './components/Popup';
-import TableauEmbed from './components/TableauEmbed';
-import { Avatar, Badge } from 'antd';
-import { BiSolidPhoneCall } from 'react-icons/bi';
-import { DndContext } from '@dnd-kit/core';
+import "./App.css";
 
-import Todo from './components/Todo';
-import { Draggable } from './components/global/Draggable';
-import { Droppable } from './components/global/Droppable';
+import Todo from "./components/Todo";
+import { Draggable } from "./components/global/Draggable";
+import { Droppable } from "./components/global/Droppable";
 
 function App() {
-  const url = 'https://public.tableau.com/app/profile/chris.gerrard/viz/RegionalSampleWorkbook/Storms';
-  const options = {
-    height: '600px',
-    width: '800px',
-    hideTabs: true,
-    hideToolbar: true,
-  };
   return (
-    <div className="app" style={{ marginTop: '20px' }}>
+    <div className='app' style={{ marginTop: "20px" }}>
       <Todo />
-      {/* <TableauEmbed url={url} options={options} /> */}
     </div>
   );
 }
